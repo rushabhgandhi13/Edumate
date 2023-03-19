26f097cbd6a2b754eeb90a29f90ae947c4325d47
Rushabh Gandhi <rushabh.dg@somaiya.edu>
2023-03-19 12:51:48 +0530

    Update settings.py

--- Edumate/settings.py
+++ Edumate/settings.py
@@ -147,5 +147,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "edumate27@gmail.com"
-EMAIL_HOST_PASSWORD = 'brkdymnwwvxeuknk'
+#EMAIL_HOST_USER = ***your email***
+#EMAIL_HOST_PASSWORD = ***your password***
